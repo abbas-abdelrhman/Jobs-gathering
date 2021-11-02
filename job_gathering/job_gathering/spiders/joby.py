e1a71efb6582767beb3b57f647344fc23d802b93
abbas-abdelrhman <54155369+abbas-abdelrhman@users.noreply.github.com>
2021-11-03 01:14:53 +0200

    First commit

---
+++ job_gathering/job_gathering/spiders/joby.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "joby"
+    # allowed_domains = ['https://wuzzuf.net']
+    start_urls = ['https://wuzzuf.net/search/jobs/?q=python&a=hpb']
+
+    rules = [
+        Rule(LinkExtractor(allow=r'wuzzuf/((?!:).)*$'), callback='parse', follow=True)
+    ]
+
+    def parse(self, response):
+        for job in response.css('div.css-pkv5jc'):
+            yield {
+                'JOB-TITLE': job.css('h2.css-m604qf a.css-o171kl::text').get(),
+                'COMPANY': job.css('div.css-d7j1kk a.css-17s97q8::text').get(),
+                'LOCATION': job.css('div.css-d7j1kk span.css-5wys0k::text').get(),
+                'LINK': job.css('a.css-o171kl::attr(href)').get(),
+
+            }
+
